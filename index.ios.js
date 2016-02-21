d2713c3832c13e3194d7f3ee4f8600ad4af3625d
vestrial <sandra.sieroux@googlemail.com>
2016-02-21 11:05:22 +0100

    shows a random run on startup

--- index.ios.js
+++ index.ios.js
@@ -11,19 +11,14 @@ import React, {
   View
 } from 'react-native';
 
+var shadowrandom = require("shadowrandom");
+
 class mrjohnson extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          {shadowrandom.randomRun()}
         </Text>
       </View>
     );
